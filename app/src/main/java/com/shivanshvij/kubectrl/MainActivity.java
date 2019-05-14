2b819cad3f26f924baf875603af67df9bffb8f20
ShivanshVij <shivanshvij@outlook.com>
2019-05-14 12:33:13 -0400

    Fixed loading bugs, removed hard-coded credentials, added ability to add credentials on first run and persist them for future runs.

--- app/src/main/java/com/shivanshvij/kubectrl/MainActivity.java
+++ app/src/main/java/com/shivanshvij/kubectrl/MainActivity.java
@@ -1,5 +1,7 @@
 package com.shivanshvij.kubectrl;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.design.widget.NavigationView;
@@ -11,7 +13,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -19,6 +24,12 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private KubeController kubecontroller;
+    SharedPreferences ClusterPreferences;
+    SharedPreferences.Editor ClusterPreferencesEditor;
+
+    NavigationView navigationView;
+
+    Boolean FirstRun = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,27 +43,51 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setSupportActionBar(toolbar);
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        this.navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-        navigationView.setNavigationItemSelectedListener(this);
+        this.navigationView.setNavigationItemSelectedListener(this);
 
-        navigationView.setCheckedItem(R.id.nav_nodes);
+        this.ClusterPreferences = getSharedPreferences("ClusterPreferences", Context.MODE_PRIVATE);
+        this.ClusterPreferencesEditor = ClusterPreferences.edit();
 
-        this.kubecontroller = new KubeController("https://142.93.145.99:6443", "eyJhbGciOiJSUzI1NiIsImtpZCI6IiJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJrdWJlLXN5c3RlbSIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VjcmV0Lm5hbWUiOiJhZG1pbi11c2VyLXRva2VuLXB4cXZoIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQubmFtZSI6ImFkbWluLXVzZXIiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC51aWQiOiIyMDIyN2I4My03NjAxLTExZTktYTdhNC0wYWE4MDliMjI0NTQiLCJzdWIiOiJzeXN0ZW06c2VydmljZWFjY291bnQ6a3ViZS1zeXN0ZW06YWRtaW4tdXNlciJ9.LBjffq_R443JGPR8jIkehlREdtFLye8Ecw8aU3dndOxsvAUAS0YGBkcxjvBQPdKzr1-KVGd73quckAg2GPJkb1ws0A0r6ZOVijC1hqkbuc3F7nyeu43PA8OoXcW_rnmZemR_YErIc2tbKVyrMHAg6sbz5yxM7L2MbwhPSjDVlsQKmKbJ6uwjlPPHKSavXVUUGlHgNJ4athzsLV2pQhfhxergB1M1XjlwQy9rH0fU2nteehclA2k25wWwNHPyphsPtpihobViZhL8p2KuH2DbSClSpa9r-IxGHa-DKHNG0wYymAXJ8gT80LyPocAD-sY0M3VlhPTlFlIpDdMg8A_31Q", false);
+        String HOSTPATH = this.ClusterPreferences.getString("HOSTPATH", "");
+        String BEARER = this.ClusterPreferences.getString("BEARER", "");
 
-        this.nodeView();
+        if(!HOSTPATH.equalsIgnoreCase("") && !BEARER.equalsIgnoreCase("")){
+            System.out.println(HOSTPATH);
+            System.out.println(BEARER);
+            this.kubecontroller = new KubeController(HOSTPATH, BEARER, false);
+
+            this.navigationView.setCheckedItem(R.id.nav_nodes);
+
+            Toast.makeText(getApplicationContext(),"Getting Nodes",Toast.LENGTH_SHORT).show();
+            this.clearVisibility();
+            this.nodeView();
+            Toast.makeText(getApplicationContext(),"Nodes successfully retrieved",Toast.LENGTH_LONG).show();
+        }
+        else{
+            this.navigationView.setCheckedItem(R.id.nav_cluster_settings);
+
+            this.FirstRun = true;
+            this.clearVisibility();
+            this.settingsClusterView();
+        }
+
+
     }
 
 
     private void clearVisibility(){
         ListView item0 = findViewById(R.id.nodelist);
         ListView item1 = findViewById(R.id.podslist);
+        LinearLayout item2 = findViewById(R.id.cluster_settings_layout);
 
         item0.setVisibility(View.GONE);
         item1.setVisibility(View.GONE);
+        item2.setVisibility(View.GONE);
     }
 
     private void nodeView(){
@@ -73,6 +108,48 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         simpleList.setVisibility(View.VISIBLE);
     }
 
+    private void settingsClusterView(){
+        LinearLayout linearLayout = findViewById(R.id.cluster_settings_layout);
+
+        EditText hostname = findViewById(R.id.hostpath_entry);
+        EditText bearer_token = findViewById(R.id.bearer_token_entry);
+
+        String HOSTPATH = this.ClusterPreferences.getString("HOSTPATH", "");
+        String BEARER = this.ClusterPreferences.getString("BEARER", "");
+
+        if(!HOSTPATH.equalsIgnoreCase("") && !BEARER.equalsIgnoreCase("")){
+            hostname.setText(HOSTPATH);
+            bearer_token.setText(BEARER);
+        }
+        else{
+            hostname.setText("NOTSAVED");
+            bearer_token.setText("NOTSAVED");
+        }
+
+        linearLayout.setVisibility(View.VISIBLE);
+    }
+
+    public void saveSettingsClusterView(View view){
+        EditText hostname = findViewById(R.id.hostpath_entry);
+        EditText bearer_token = findViewById(R.id.bearer_token_entry);
+
+        this.ClusterPreferencesEditor.clear();
+
+
+        this.ClusterPreferencesEditor.putString("HOSTPATH", hostname.getText().toString());
+        this.ClusterPreferencesEditor.putString("BEARER", bearer_token.getText().toString());
+        this.ClusterPreferencesEditor.commit();
+
+        Toast.makeText(getApplicationContext(),"New Credentials successfully saved",Toast.LENGTH_LONG).show();
+
+        if(this.FirstRun){
+            this.FirstRun = false;
+            this.kubecontroller = new KubeController(hostname.toString(), bearer_token.toString(), false);
+            this.navigationView.setCheckedItem(R.id.nav_nodes);
+        }
+
+    }
+
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -90,12 +167,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         int id = item.getItemId();
 
         if (id == R.id.nav_nodes) {
+            Toast.makeText(getApplicationContext(),"Getting Nodes",Toast.LENGTH_SHORT).show();
             this.clearVisibility();
             this.nodeView();
+            Toast.makeText(getApplicationContext(),"Nodes successfully retrieved",Toast.LENGTH_LONG).show();
 
         } else if (id == R.id.nav_pods) {
+            Toast.makeText(getApplicationContext(),"Getting Pods",Toast.LENGTH_SHORT).show();
             this.clearVisibility();
             this.podsView();
+            Toast.makeText(getApplicationContext(),"Pods successfully retrieved",Toast.LENGTH_LONG).show();
 
         } else if (id == R.id.nav_services) {
             this.clearVisibility();
@@ -121,7 +202,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
         else if (id == R.id.nav_cluster_settings){
             this.clearVisibility();
-
+            this.settingsClusterView();
 
         }
 
